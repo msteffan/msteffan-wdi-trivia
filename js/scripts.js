acf7db177e90479239c79e3c1814574c73602932
Melissa Steffan <msteffan2012@gmail.com>
2015-07-13 16:53:43 -0400

    refactored js code into object

--- js/scripts.js
+++ js/scripts.js
@@ -3,89 +3,128 @@
 // $("#DC").on("click", playGame);
 
 // Global Variables:
-var num = 0;
-var timer = {
-    seconds: 180,
-    newTime: 0,
-    increment: function incrementTime() {
-        if(timer.seconds>0){
-            setInterval(function(){
-                timer.seconds--;
-                $("#timer").html(timer.seconds);
-            }, 1000)
-        } else {
-            alert("Timer is up!");
-        };
-    }
-};
-var qCounter = {
-    numCorrect: 0,
-    totalQ: 15,
-    numAsked: 0,
-    addCorrect: function incrementQs(){
-        qCounter.numCorrect++
-        $("p.correctAnswers").html(this.numCorrect);
-    },
-    getTotal: function checkRemaining() {
-        qCounter.numAsked++;
-        var numRemaining = qCounter.totalQ - qCounter.numAsked;
-        $(".numQuestionsLeft").html(numRemaining)
-    }
-};
-var answers = {
-    Seattle: ["Pike Place","Columbia Tower","Greenlake", "Mount Rainier", "true", "true", "Fremont Troll", "UW", "false", "Hempfest", "Starbuck","Seahawks", "false", "12th Man", "Microsoft", "Boeing"],
-    DC: ["Metro", "Union Station", "false", "states", "false", "false", "36", "Washington Monument", "true", "Wizards", "Muriel Bowser", "Russell", "true", "Air and Space", "true"]
-}
+// var num = 0;
+// var timer = {
+//     seconds: 60,
+//     newTime: 0,
+//     increment: function incrementTime() {
+//         var interval = setInterval(function(){
+//             if(timer.seconds > 0){
+//                 timer.seconds--;
+//                 $("#timer").html(timer.seconds);
+//             } else {
+//                 alert("time's up")
+//                 clearInterval(interval)
+//             }
+//         }, 1000);
+//     }
+// };
+// var qCounter = {
+//     numCorrect: 0,
+//     totalQ: 15,
+//     numAsked: 0,
+//     addCorrect: function incrementQs(){
+//         qCounter.numCorrect++
+//         $("p.correctAnswers").html(this.numCorrect);
+//     },
+//     getTotal: function checkRemaining() {
+//         qCounter.numAsked++;
+//         var numRemaining = qCounter.totalQ - qCounter.numAsked;
+//         $(".numQuestionsLeft").html(numRemaining)
+//     }
+// };
+// var answers = {
+//     Seattle: ["Pike Place","Columbia Tower","Greenlake", "Mount Rainier", "true", "true", "Fremont Troll", "UW", "false", "Hempfest", "Starbuck","Seahawks", "false", "12th Man", "Microsoft", "Boeing"],
+//     DC: ["Metro", "Union Station", "false", "states", "false", "false", "36", "Washington Monument", "true", "Wizards", "Muriel Bowser", "Russell", "true", "Air and Space", "true"]
+// }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // on page load:
 
 $(window).load(function(){
     // start the timer
-    timer.increment()
+    trivia.timer.increment();
     // display questions correct out of total questions
-    $("p.correctAnswers").html(qCounter.numCorrect)
+    $("p.correctAnswers").html(trivia.qCounter.numCorrect)
     // display first question
-    $("h4.question").eq(num).css("display", "block")
+    $("h4.question").eq(trivia.num).css("display", "block")
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // game play functions
 
-function checkAnswer(){
-    event.preventDefault();
-    currentGuess = $("#playerInput").val()
-    if (currentGuess=== answers.Seattle[num])
-        qCounter.addCorrect();
-    // hides prev questions and displays the next question
-    displayAnswers();
-    showQuestion();
-    //resets input box to empty
-    $("#playerInput").val("");
-    //display next question
-    // decrease numQuestionsLeft by one
-    qCounter.getTotal();
-}
+var trivia = {
+    num: 0,
+    timer: {
+        seconds: 60,
+        newTime: 0,
+        increment: function incrementTime() {
+            var interval = setInterval(function(){
+                if(trivia.timer.seconds > 0){
+                    trivia.timer.seconds--;
+                    $("#timer").html(trivia.timer.seconds);
+                } else {
+                    console.log("time's up")
+                    clearInterval(interval)
+                }
+            }, 1000);
+        }
+    },
+    qCounter: {
+        numCorrect: 0,
+        totalQ: 15,
+        numAsked: 0,
+        addCorrect: function incrementQs(){
+            trivia.qCounter.numCorrect++
+            $("p.correctAnswers").html(this.numCorrect);
+        },
+        getTotal: function checkRemaining() {
+            trivia.qCounter.numAsked++;
+            var numRemaining = trivia.qCounter.totalQ - trivia.qCounter.numAsked;
+            $(".numQuestionsLeft").html(numRemaining)
+            if(numRemaining<0){
+                $(".numQuestionsLeft").html("Game is over!");
+            }
+        }
+    },
+    answers: {
+        Seattle: ["Pike Place","Columbia Tower","Greenlake", "Mount Rainier", "true", "true", "Fremont Troll", "UW", "false", "Hempfest", "Starbuck","Seahawks", "false", , "Microsoft", "Boeing"],
+        DC: ["Metro", "Union Station", "false", "states", "false", "false", "36", "Washington Monument", "true", "Wizards", "Muriel Bowser", "Russell", "true", "Air and Space", "true"]
+    },
+    playGame: {
+        checkAnswer: function checkAnswer() {
+            event.preventDefault();
+            currentGuess = $("#playerInput").val();
+            if (currentGuess === trivia.answers.Seattle[trivia.num])
+                trivia.qCounter.addCorrect();
+            // hides prev questions and displays the next question
+            trivia.playGame.displayAnswers();
+            trivia.playGame.showQuestion();
+            //resets input box to empty
 
-function displayAnswers(){
-    $(".showAnswers").append("<tr class='answersRow'></tr>");
-    $(".answersRow").last().append("<td>"+currentGuess+"</td>")
-    $(".answersRow").last().append("<td>"+answers.Seattle[num]+"</td>")
-}
-
-function showQuestion(){
-    if(num+1 >= $("h4.question").length){
-        lastQuestion();
+            //display next question
+            // decrease numQuestionsLeft by one
+            trivia.qCounter.getTotal();
+            $("#playerInput").val("");
+        },
+        displayAnswers: function displayAnswers() {
+            $(".showAnswers").append("<tr class='answersRow'></tr>");
+            $(".answersRow").last().append("<td>"+ currentGuess + "</td>")
+            $(".answersRow").last().append("<td>"+trivia.answers.Seattle[trivia.num]+"</td>")
+        },
+        showQuestion: function showQuestion() {
+            if(trivia.num+1 >= $("h4.question").length){
+                trivia.playGame.lastQuestion();
+            }
+            $("h4.question").eq(trivia.num).css("display", "none");
+            trivia.num++;
+            $("h4.question").eq(trivia.num).css("display", "block");
+        },
+        lastQuestion: function lastQuestion() {
+            $("#lastSeattle").after("<h4 class='lastQuestion' style='display:block'></h4>");
+            $(".lastQuestion").html("You answered "+ trivia.qCounter.numCorrect + " out of 15 questions correctly!");
+        }
     }
-    $("h4.question").eq(num).css("display", "none");
-    num++;
-    $("h4.question").eq(num).css("display", "block");
-
-}
-
-function lastQuestion() {
-    $("h4.question").last().after("<h4 class='lastQuestion' style='display:block'></h4>");
-    $(".lastQuestion").html("You answered "+ qCounter.numCorrect + " out of 15 questions correctly!");
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,6 +134,6 @@ function lastQuestion() {
 $("#playerInput").on("keypress", function(e){
 //  event.preventDefault();
     if(e.which === 13){
-        checkAnswer();
+        trivia.playGame.checkAnswer();
     }
 });
